5fa8e5ce5a21831630ff938e93f897e259a5bcd2
lzpong <lzpong@163.com>
2022-09-22 16:36:04 +0800

    Update libdownload.js

--- Node_HTTP或HTTPS异步下载工具类/libdownload.js
+++ Node_HTTP或HTTPS异步下载工具类/libdownload.js
@@ -5,13 +5,15 @@ const {statSync,existsSync,mkdirSync,appendFile,writeFile,readFileSync}=require(
 /*
 HTTP或HTTPS异步下载工具类
 支持匿名代理; 或用户名加密码代理
-回调函数: callback(data<ArrayBuffer>,statusCode,headers)
+回调函数: callback(data<ArrayBuffer>,statusCode,headers,spen)
+回调函数: downcallback(bool,url)
 */
 module.exports={
-    downToFile:downToFile, //异步下载文件(默认尝试3次,如果 savePath 存在且不小于 1KB 则跳过): 调用: `downToFile(url,savePath,callback,tryCount)`
-    tryDown:tryDown,  //异步下载文件(默认尝试3次): 调用: `downToFile(url,savePath,callback,tryCount)`
+    downToFile:downToFile, //异步下载文件(默认尝试3次,如果 savePath 存在且不小于 1KB 则跳过): 调用: `downToFile(url,savePath,downcallback,tryCount)`
+    tryDown:tryDown,  //异步下载文件(默认尝试3次): 调用: `downToFile(url,savePath,downcallback,tryCount)`
     setPostHeaders:setPostHeaders,
     setGetHeaders:setGetHeaders,
+    setResponsTimeout:setResponsTimeout,//服务器响应超时
     request:request, //异步http/https请求 调用: `request(method,url,data,headers,fun)`
     post:post, //异步POST请求 调用: `post(url,data,callback)`
     get:get,   //异步POST请求 调用: `get(url,data,callback)`
@@ -41,9 +43,11 @@ var defaultHeaders={"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) App
 var postHeaders=defaultHeaders;
 var getHeaders=defaultHeaders;
 var proxy=null,auth=null;
+var responsTimeout=2*60*1000;//服务器响应超时,默认2分钟
 
-function setPostHeaders(hd) { postHeaders=hd;}
-function setGetHeaders(hd) { getHeaders=hd;}
+function setPostHeaders(hd) {postHeaders=hd}
+function setGetHeaders(hd) {getHeaders=hd}
+function setResponsTimeout(ms){responsTimeout=ms}
 
 function getCli(url){
     if(proxy)
@@ -53,9 +57,13 @@ function getCli(url){
 }
 
 function setProxy(url,user,pswd){
-    proxy=p;
-    if(user!=null && user!="")
-        auth="Basic "+new Buffer('flyingzl:password').toString('base64')
+    proxy=url;
+    if(user!=null && user!=""){
+        if(pswd!=null && pswd!="")
+            auth="Basic "+new Buffer(user+':'+pswd).toString('base64');
+        else
+            auth=user;
+    }
 }
 
 //http请求 callback(data<ArrayBuffer>,statusCode,headers)
@@ -82,23 +90,40 @@ function request(method,url,data,headers,fun) {
         if(ops.headers["Content-Type"]==null || ops["Content-Type"]=="")
             ops.headers["Content-Type"]= "application/x-www-form-urlencoded; charset=UTF-8";
     }
+    // 请求1分钟超时
+    request_timer = setTimeout(function() {
+        console.error(getTime()+"=>"+method+" "+url+" Request Timeout  spen:"+(new Date().getTime()-startTime)/1000+"s");
+        req.destroy(new Error("Request Timeout"));
+    }, 60000);
+    var startTime=new Date().getTime();
     var httpcli=getCli(url);
     var req=httpcli.request(ops,(res)=>{
       var st=res.statusCode;
       var hd=res.headers;
       var list=[];
       var size=0;
+      clearTimeout(request_timer);
+      // 响应超时
+      response_timer = setTimeout(function() {
+          console.error(getTime()+"=>"+method+" "+url+" Response Timeout  spen:"+(new Date().getTime()-startTime)/1000+"s");
+          req.destroy(new Error("Response Timeout"));
+      }, responsTimeout);
       res.on('data', (d) => {
           list.push(d);
           size+=d.length;
       }).on('end',()=> {
-          fun&&fun(Buffer.concat(list,size),st,hd);
+          clearTimeout(response_timer);
+          fun&&fun(Buffer.concat(list,size),st,hd,new Date().getTime()-startTime);
       });
     });
     req.on('error', (e) => {
-        console.error("=>"+method+" Error "+url+"\n",e);
-        fun&&fun(e);
+        let sp=new Date().getTime()-startTime;
+        console.error(getTime()+"=>"+method+" "+url+" "+e+" spen:"+(sp/1000)+"s\n");
+        fun&&fun(e,null,null,sp);
     });
+    //设置个超时 60s
+    req.socket&&req.socket.setTimeout(60000);
+    req.connection&&req.connection.setTimeout(60000);
     if(method=="POST"&&data!=null)
         req.write(data);
     req.end();
@@ -114,58 +139,56 @@ function get(url,fun){
     request("GET",url,null,getHeaders,fun);
 }
 
-//下载到文件 
+//下载尝试
 function tryDown(url,path,fun,timeoutRetry){
     if(url==null || url==''){
-      console.error("=>`url` param is Empty");
-      fun&&fun(true);
+      console.error(getTime()+"=>`url` param is Empty");
+      fun&&fun(true,url);
     }
-    get(url,(data,st,hd)=>{
+    get(url,(data,st,hd,sp)=>{
+        sp=sp/1000+'s';
         hd=hd||{"content-type":"<content-error>","content-length":0,"location":""};
         if(st!=200){
-            console.error("=>Error: down "+path+" "+url+"   "+st+" "+ hd['content-type']);
-            log("=>Error: down "+path+" "+url+"   "+st+"\n");
+            if(st==404) data="Not Found";
+            console.error(getTime()+"=>DOWN "+url+" Error.  "+st+" "+" spen:"+sp+"\n",data);
+            log("=>DOWN "+url+" "+data+" "+st+"  spen:"+sp+"\n");
             if(st!=404){
-                log(JSON.stringify(hd,"    "));
-                log("\n");
-                log(data);
-                log("\n");
                 if(st==301 || st==302)
                   if(hd["location"].startsWith("http"))
                     url=hd["location"];
             }
-            if(--timeoutRetry>0 && data.code=="ETIMEDOUT")
+            if(--timeoutRetry>0 && (data.code=="ETIMEDOUT"||data.code=="ECONNRESET"))
                 tryDown(url,path,fun,timeoutRetry);
             else
-                fun&&fun(false);
+                fun&&fun(false,url);
         }
         else{
             let len=hd['content-length']||-1;
-            console.log("=>down  "+path+" "+url+"  "+hd['content-type']+"  len: "+len+"\n");
+            console.info(getTime()+"=>DOWN "+url+" OK  "+path+"  "+hd['content-type']+"  len: "+len+"  spen:"+sp);
             //console.log(data);
             //log("\n"+data+"\n");
-            log("=>down  "+path+"  "+url+"  "+hd['content-type']+"  len: "+len+"\n");
+            log("=>DOWN "+url+" OK  "+path+"   "+hd['content-type']+"  len: "+len+" spen:"+sp+"\n");
             if(len<0)
-                return fun&&fun(true),null;
+                return fun&&fun(true,url);
             let l=path.lastIndexOf('/');
             if(l<0) l=path.lastIndexOf('\\');
             if(l>1) mkdir(path.substr(0,l));
 
             writeFile(path,data,(err)=>{
                 if(err){
-                    log("=>Error: writeFile "+path+" "+url+"   "+err+"\n");
-                    console.error("=>Error: writeFile "+path,url,err);
+                    log("=>DOWN "+url+" Error: writeFile "+path+"   "+err+"\n\n");
+                    console.error(getTime()+"=>DOWN "+url+" Error: writeFile "+path,url,err);
                 }
             });
-            fun&&fun(true);
+            fun&&fun(true,url);
         }
     });
 }
 function downToFile(url,path,fun,tryCount){
     let st={size:0};
     if(url==null || url==''){
-      console.error("=>`url` param is Empty");
-      fun&&fun(true);
+      console.error(getTime()+"=>`url` param is Empty");
+      fun&&fun(true,url);
     }
     if(existsSync(path))
         st=statSync(path);
@@ -173,7 +196,7 @@ function downToFile(url,path,fun,tryCount){
         tryDown(url,path,fun,tryCount||3);//默认尝试3次
     }
     else
-        fun&&fun(true);
+        fun&&fun(true,url);
 }
 
 function unescape(path){
@@ -213,14 +236,17 @@ function exists(path){
 }
 
 var logfile;
-function logFileChange(){logfile="log_"+(new Date()).toLocaleString().replace(/[\:\-\/]/g,'').replace(' ','_')+".log"}
+function logFileChange(){mkdir("log");logfile="log/log_"+(new Date()).toISOString().replace(/[\:\-\/Z]/g,'').replace(/[ T]/g,'_')+".log"}
 logFileChange();
 function log(data){
-    let d=new Date();
-    let t=d.toTimeString();
-    t=t.substr(0,t.indexOf(' '));
-    appendFile(logfile,t+" "+data,(err)=>{if(err)appendFileSync(logfile,t+" "+data)});
+    if(data=="\n")
+        appendFile(logfile,data);
+    else{
+        let t=getTime()
+        appendFile(logfile,t+data,(err)=>{if(err)appendFileSync(logfile,t+data)});
+    }
 }
+function getTime(){ return new Date().toTimeString().substr(0,9);}
 
 /**
 * 异步延迟
